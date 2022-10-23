3a62757f66546e472384ac53618e224a0be180b6
Nadia Goh <54729791+nytgoh@users.noreply.github.com>
2022-10-23 15:17:13 +1300

    final touches to calculations and css elements

--- animal-age-converter-backEnd/Controllers/AnimalAgeController.cs
+++ animal-age-converter-backEnd/Controllers/AnimalAgeController.cs
@@ -10,7 +10,7 @@ namespace animal_age_converter_backEnd.Controllers
          * One human year is equal to the stored years in animal
          * if there are multiple, each item will count for 1 year at the start of their life
          * the last item will count for the rest of the years post the others
-         * E.g. Dog = [15, 9, 4]
+         * E.g. if Dog = [15, 9, 4]
          * 15 dog year = 1st human year
          * 9 dog year = 2nd human year
          * 4 dog year = each 1 human year after the 2nd year
@@ -18,13 +18,13 @@ namespace animal_age_converter_backEnd.Controllers
         private Dictionary<string, IList<double>> ConversionPerHumanYearBracket = new Dictionary<string, IList<double>>()
         {
             { "human", new List<double>() {1} },
-            { "rabbit", new List<double>() {8.89}},
-            { "cat", new List<double>() { 3.2 } },
+            { "rabbit", new List<double>() { 20, 6 }},
+            { "cat", new List<double>() { 12.5, 12.5, 4 } },
             { "dog", new List<double>() { 15, 9 , 5, 4 } },
-            { "pig", new List<double>() { 3.2 } },
-            { "horse", new List<double>() { 2 } },
-            { "monkey", new List<double>() { 3.2 } },
-            { "hamster", new List<double>() { 20 } }
+            { "pig", new List<double>() { 18, 4 } },
+            { "horse", new List<double>() { 6, 6, 6, 2.5 } },
+            { "monkey", new List<double>() { 4, 4, 4, 3 } },
+            { "hamster", new List<double>() { 26 } }
         };
 
         // If human age is input, another animal name should be input to convert the human age into
